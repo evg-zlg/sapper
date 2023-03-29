a50c244ba09464641b7b5d55664d1f66d7a85214
evg-zlg <evg.zalogin@gmail.com>
2023-03-29 12:56:10 +0500

    fix: delete unused vars

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { Cell } from './Cell';
 import { GameMenu } from './GameMenu';
