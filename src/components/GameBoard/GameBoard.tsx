277ef4a801c41b53a5c87cceb19e8a05aa0ffff0
evg-zlg <evg.zalogin@gmail.com>
2023-03-30 20:34:50 +0500

    fix: resolve conflitcs

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { boardSize } from "../../const/const";
-import { createGrid } from "./utils";
-
-interface IBoardProps {
-  col: number;
-  row: number;
-}
-const Board = styled.section<IBoardProps>`
-  border-radius: 5px;
-  border: 1px solid var(--primary-dark-color);
-`;
-
-function GameBoard() {
-  const [size] = useState(boardSize[0]);
-  const [gameGrid] = useState(() => createGrid(size));
-
-  useEffect( () => {
-    console.log(gameGrid);
-  }, [size]);
-
-  return (
-    <>
-      <header>time, button for restart, and someone else</header>
-      <Board col={size.col} row={size.row} />
-    </>
-=======
 import { useEffect } from 'react';
 import styled from 'styled-components';
 import { Cell } from './Cell';
@@ -231,7 +202,6 @@ function GameBoard() {
         </Frame>
       </Board>
     </Frame>
->>>>>>> 0d067eadc5f56363afd1b77b2594107c4fb96ac5
   );
 }
 
